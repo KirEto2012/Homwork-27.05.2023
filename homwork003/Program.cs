1f14e94ec6143d9c8f9efc509dd323babeaf5791
«liya» <ilialisov8410@gmail.com>
2023-05-27 12:43:18 +0700

    homwork001.27.05.2023

---
+++ homwork003/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+4 -> да
+-3 -> нет
+7 -> нет
+*/
+Console.WriteLine("Ввидите число");
+int numberA = int.Parse(Console.ReadLine());
+
+if (numberA % 2 == 1)
+{
+    Console.WriteLine("Число - (" + numberA + ") НЕЧЁТНОЕ ");
+}
+else
+{
+    Console.WriteLine("Число - (" + numberA + ") ЧЁТНОЕ ");
+}
---
+++ homwork004/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+5 -> 2, 4
+8 -> 2, 4, 6, 8
+*/
+
+int i = 1;
+bool not = true;
+
+Console.WriteLine("Впишыте число:");
+int numberA = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Чётное число от 1 до " + numberA);
+while (i <= numberA)
+{
+    if (i % 2 != 1)
+    {
+        Console.Write(i + ", ");
+        not = false;
+    }
+    i++;
+}
+
+if (not)
+{
+    Console.WriteLine("Нечётное число");
+}
+
